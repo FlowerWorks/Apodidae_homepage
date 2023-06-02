8d30aed7f7ef2af250be3cf5f7a2c7077e81b0da
纳兰音韵 <nalanyinyun@outlook.com>
2023-06-02 21:45:17 +0800

    add logo

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -7,17 +7,13 @@ export default {
       { text: "Download", link: "https://github.com/naranyinyun/Apodidae/releases/latest" },
       { text: "Contribution", link: "/contribution"},
       { text: "Guide", link: "/guide"},
-   head: [
-     ['link', { rel: 'icon', herf:'back.svg' }],
-   ],
     ],
     footer: {
       message: "Open Source under the GPL_V3 License.",
       copyright: "Copyright 2022-2023 Nalanyinyun & Lily Works | <a href='https://status.nalanyinyun.top/'>Status</a>"
     },
     head: [
-      'script',
-      {defer: '', src: 'https://static.cloudflareinsights.com/beacon.min.js', 'data-cf-beacon': '{"token": "edb2140bf87349168acde21e5b887e05"}'}
+      ['link', { rel: 'icon', herf:'back.svg' }],
     ],
     },
   }
